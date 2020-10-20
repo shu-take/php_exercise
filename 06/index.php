fffd64d1afac8f554a83ccbef2dfe5b5465b997c
shu-take <shingen9@gmail.com>
2020-10-20 23:54:01 +0900

    課題2オブジェクト指向の練習 完了

--- 06/index.php
+++ 06/index.php
@@ -1,7 +1,5 @@
 <?php
 
-// 以下にCarクラスを定義して、当プログラムを実行した時にエラーがでないようにして下さい。
-
 class Car
 {
     private $name;
@@ -52,43 +50,72 @@ class Car
     }
 }
 
-// 以下を実行した時にエラーがでないようにして下さい。
+class Taxi extends Car
+{
+    private $passenger = 0;
+
+    public function infomation()
+    {
+        return parent::infomation().'乗車人数は'.$this->passenger.'人です';
+    }
+
+    public function error($num)
+    {
+        if($num > 0 && is_int($num)){
+            return true;
+        }
+    }
+
+    public function pickup($num)
+    {
+        if($this->error($num)){
+            $this->passenger += $num;
+        }else{
+            echo '降車人数に誤りがあります';
+        }
+        
+    }
+
+    public function lower($num)
+    {
+        if($this->error($num)){
+            if(($this->passenger - $num) >= 0){
+                $this->passenger -= $num;
+                echo $num.'人降車しました';
+            }else{
+                echo '降車人数に誤りがあります';
+            }
+        }else{
+            echo '降車人数に誤りがあります';
+        }
+        
+    }
+
+}
+
+
 // Carクラスのインスタンスを生成
 // 引数 : 車名、車体番号、カラー
 $car1 = new Car('ランクル', 100, 'black');
 
-// 車名(車種)を取得
-echo $car1->getName();
-echo '<br>';
-
-
-// 車体番号を取得
-echo $car1->getNumber();
-echo '<br>';
-
-// カラーを取得
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
+// 車の情報を表示
 echo $car1->infomation();
 echo '<br>';
+echo '<hr>';
 
-// 車名(車種)を更新
-$car1->setName('アルファード');
-echo $car1->getName();
+// Taxiクラスのインスタンスを生成
+$taxi1 = new Taxi('クラウンのタクシー', 222, 'black');
+
+// 3人乗車
+$taxi1->pickUp(3);
+
+// タクシーの情報表示を表示
+echo $taxi1->infomation();
 echo '<br>';
 
-// 車体番号を取得
-$car1->setNumber(200);
-echo $car1->getNumber();
+// 2人降車
+$taxi1->lower(2);
 echo '<br>';
 
-// カラーを取得
-$car1->setColor('red');
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
-echo $car1->infomation();
-echo '<br>';
+// 一人降車
+$taxi1->lower(2);
