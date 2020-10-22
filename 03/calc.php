632e258835fb0ba8f9415c34f889162bd59c8146
shu-take <shingen9@gmail.com>
2020-10-22 22:13:21 +0900

    03/calc.php

--- 03/calc.php
+++ 03/calc.php
@@ -6,19 +6,19 @@
 
     if($operator == 'addition'){
         $add = $num1 + $num2;
-        echo $num1.' + '.$num2.' = '.$add;
+        echo $num1 . ' + ' . $num2 . ' = ' . $add;
     }
     elseif($operator == 'subtraction'){
         $sub = $num1 - $num2;
-        echo $num1.' - '.$num2.' = '.$sub;
+        echo $num1 . ' - ' . $num2 . ' = ' . $sub;
     }
     elseif($operator == 'multiplication'){
         $mul = $num1 * $num2;
-        echo $num1.' * '.$num2.' = '.$mul;
+        echo $num1 . ' * ' . $num2 . ' = ' . $mul;
     }
     elseif($operator == 'division'){
         $div = $num1 / $num2;
-        echo $num1.' / '.$num2.' = '.$div;
+        echo $num1 . ' / ' . $num2 . ' = ' . $div;
     }
     else{
         echo '正しい演算子を指定して下さい';
