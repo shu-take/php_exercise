03f8e9e4c96fedb61c6d98183cc21fd4fc33a87c
shu-take <shingen9@gmail.com>
2020-10-23 01:13:03 +0900

    課題_if if_2.php 修正

--- 02/if_1.php
+++ 02/if_1.php
@@ -1,10 +1,13 @@
 <?php
 
     $a = 5;
-    $b = 3;
+    $b = 5;
 
     if($a > $b){
         echo $a;
-    }elseif($a == $b){
+    }elseif($a < $b){
+        echo $b;
+    }
+    else{
         echo '入力された値は等しいです';
     }
