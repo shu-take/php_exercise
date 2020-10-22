ff5480b4df765e2c6aab3ea7f293e5c09c1b7d1f
shu-take <shingen9@gmail.com>
2020-10-23 01:27:44 +0900

    課題_if if_2.php 修正

--- 02/if_2.php
+++ 02/if_2.php
@@ -2,9 +2,9 @@
 
     $point = 4;
 
-    if($point % 2 == 0){
+    if ($point % 2 == 0) {
         echo '偶数です';
     }
-    elseif($point % 2 != 0 ){
+    elseif ($point % 2 != 0 ) {
         echo '奇数です';
     }
